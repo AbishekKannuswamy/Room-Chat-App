dba3d4cb2744c8ebce682a78d20cba27eeb496b7
AbishekKannuswamy <abishekkannuswamy@gmail.com>
2024-09-30 23:44:11 +0530

    kk

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
+# Initialize SocketIO
 socketio = SocketIO()
-
 
 def create_app(debug=False):
     """Create an application."""
@@ -10,9 +10,11 @@ def create_app(debug=False):
     app.debug = debug
     app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
 
+    # Register blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    socketio.init_app(app)
-    return app
+    # Initialize SocketIO with CORS support
+    socketio.init_app(app, cors_allowed_origins="*")  # Enable CORS for all origins
 
+    return app
