63087fb45cd7bacbe87932ed1978e32c47985f2e
AbishekKannuswamy <abishekkannuswamy@gmail.com>
2024-09-30 21:58:17 +0530

    t

--- chat.py
+++ chat.py
@@ -2,8 +2,6 @@
 from app import create_app, socketio
 
 app = create_app(debug=True)
-socketio.init_app(app)
 
-# Remove this block
-# if __name__ == '__main__':
-#     socketio.run(app)
+if __name__ == '__main__':
+    socketio.run(app)
